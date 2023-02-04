de8d46aca048f2b880055aef274acad82ba3af5f
Rituraj Ingale <ritu.raj.ingale@gmail.com>
2023-02-05 00:45:28 +0530

    setNewStoryInDB function clean up: removed unnecessary data from schema

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -14,15 +14,11 @@ mongoose.connect(`mongodb+srv://${dbUsername}:${dbPassword}@hacker-news-rituraj.
 const storySchema = new Schema({
     _id: Schema.Types.Number,
     storyId: Schema.Types.Number,
-    type: Schema.Types.String,
     by: Schema.Types.String,
     time: Schema.Types.Date,
-    parent: Schema.Types.Number,
-    kids: Schema.Types.Array,
     url: Schema.Types.String,
     score: Schema.Types.Number,
     title: Schema.Types.String,
-    descendants: Schema.Types.Number,
 });
 
 const Story = mongoose.model('Story', storySchema);
@@ -32,30 +28,22 @@ const setNewStoryInDB = (list) => {
     list.forEach(async (current) => {
         const {
             id,
-            type,
             by,
             time,
-            parent,
-            kids,
             url,
             score,
             title,
-            descendants,
         } = current;
         const isNew = !(await Story.exists({ _id: id }));
         if (isNew) {
             const story = new Story({
                 _id: id,
                 storyId: id,
-                type,
                 by,
                 time,
-                parent,
-                kids,
                 url,
                 score,
                 title,
-                descendants,
             });
             story.save((e) => {
                 if (e) {
